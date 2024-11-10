89ca7c3dc4cc7e89f8122423e79e7e769a76f544
repparw <ubritos@gmail.com>
2024-11-09 22:20:47 -0300

    test ruby nix

--- flake.nix
+++ flake.nix
@@ -1,52 +1,93 @@
 {
+  description = "A simple ruby app demo";
+
+  nixConfig = {
+    extra-substituters = "https://nixpkgs-ruby.cachix.org";
+    extra-trusted-public-keys = "nixpkgs-ruby.cachix.org-1:vrcdi50fTolOxWCZZkw0jakOnUI1T19oYJ+PRYdK4SM=";
+  };
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-ruby.url = "github:bobvanderlinden/nixpkgs-ruby";
-    nixpkgs-ruby.inputs.nixpkgs.follows = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-    bundix.url = "github:nix-community/bundix";
+    nixpkgs.url = "nixpkgs";
+    ruby-nix.url = "github:inscapist/ruby-nix";
+    # a fork that supports platform dependant gem
+    bundix = {
+      url = "github:inscapist/bundix/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fu.url = "github:numtide/flake-utils";
+    bob-ruby.url = "github:bobvanderlinden/nixpkgs-ruby";
+    bob-ruby.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      nixpkgs-ruby,
-      flake-utils,
+      fu,
+      ruby-nix,
+      bundix,
+      bob-ruby,
     }:
-    flake-utils.lib.eachDefaultSystem (
+    with fu.lib;
+    eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        ruby = nixpkgs-ruby.lib.packageFromRubyVersionFile {
-          file = ./.ruby-version;
+        pkgs = import nixpkgs {
           inherit system;
+          overlays = [ bob-ruby.overlays.default ];
         };
+        rubyNix = ruby-nix.lib pkgs;
 
-        gems = pkgs.bundlerEnv {
-          name = "gemset";
-          inherit ruby;
-          gemfile = ./Gemfile;
-          lockfile = ./Gemfile.lock;
-          gemset = ./gemset.nix;
-          groups = [
-            "default"
-            "production"
-            "development"
-            "test"
-          ];
-        };
+        # TODO generate gemset.nix with bundix
+        gemset = if builtins.pathExists ./gemset.nix then import ./gemset.nix else { };
+
+        # If you want to override gem build config, see
+        #   https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/ruby-modules/gem-config/default.nix
+        gemConfig = { };
+
+        # See available versions here: https://github.com/bobvanderlinden/nixpkgs-ruby/blob/master/ruby/versions.json
+        ruby = pkgs."ruby-3.3.1";
+
+        # Running bundix would regenerate `gemset.nix`
+        bundixcli = bundix.packages.${system}.default;
+
+        # Use these instead of the original `bundle <mutate>` commands
+        bundleLock = pkgs.writeShellScriptBin "bundle-lock" ''
+          export BUNDLE_PATH=vendor/bundle
+          bundle lock
+        '';
+        bundleUpdate = pkgs.writeShellScriptBin "bundle-update" ''
+          export BUNDLE_PATH=vendor/bundle
+          bundle lock --update
+        '';
       in
-      {
-        devShell =
-          with pkgs;
-          mkShell {
-            buildInputs = [
-              gems
-              bundix
-              bundix.default
-            ];
+      rec {
+        inherit
+          (rubyNix {
+            inherit gemset ruby;
+            name = "my-rails-app";
+            gemConfig = pkgs.defaultGemConfig // gemConfig;
+          })
+          env
+          ;
+
+        devShells = rec {
+          default = dev;
+          dev = pkgs.mkShell {
+            buildInputs =
+              [
+                env
+                bundixcli
+                bundleLock
+                bundleUpdate
+              ]
+              ++ (with pkgs; [
+                yarn
+                rufo
+                # more packages here
+              ]);
           };
+        };
       }
     );
 }
