cbf7408cd97dba617fd2d056e6679f9a92c29e08
repparw <ubritos@gmail.com>
2024-11-14 15:48:46 -0300

    dark mode, favicon, logo

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -4,5 +4,10 @@ module Public
       @products = Product.available.limit(20).order(created_at: :desc)
       @categories = Category.all
     end
+
+    def show
+     @product = Product.find(params[:id])
+    end
   end
 end
+
